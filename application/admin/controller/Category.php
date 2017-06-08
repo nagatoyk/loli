405f4c0e654fa7b89c664baf3112b8aec5a270e0
凉宫长门 <393117228@qq.com>
2017-06-09 06:39:42 +0800

    XG
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- application/admin/controller/Category.php
+++ application/admin/controller/Category.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace app\admin\controller;
+namespace app\Admin\controller;
 
 class Category extends Common
 {
--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace app\admin\controller;
+namespace app\Admin\controller;
 
 class Index extends Common
 {
