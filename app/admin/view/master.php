23ba96cd02d1cc800fffadb00e00d7227375d597
凉宫长门 <393117228@qq.com>
2017-06-09 05:48:52 +0800

    修改
    Signed-off-by: 凉宫长门 <393117228@qq.com>

---
+++ app/admin/view/master.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/6/8
+ * Time: 19:40
+ */
+?>
+<html>
+<head>
+    <title>123</title>
+</head>
+<body>
+<blade name="content"/>
+</body>
+</html>
