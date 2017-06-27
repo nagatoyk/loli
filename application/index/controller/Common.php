4319669c8b1b20b949680d13f144c4ba8d99e040
凉宫长门 <393117228@qq.com>
2017-06-27 08:57:38 +0800

    修改
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- application/index/controller/Common.php
+++ application/index/controller/Common.php
@@ -10,6 +10,8 @@ class Common extends Controller
     public function __construct(Request $request = null)
     {
         parent::__construct($request);
+        $list = glob('*');
+        dump($list);
         $this->assign('cate', model('Category')->where('des','<>','')->order('id','asc')->select());
         $url = 'https://hitoapi.cc/s/?_='.time();
         $json = json_decode(file_get_contents($url), true);
