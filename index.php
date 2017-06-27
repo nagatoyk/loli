a343010aec640ebef45d37b421ac66c6b1bbae4c
凉宫长门 <393117228@qq.com>
2017-06-28 07:27:01 +0800

    修改
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
-echo 123;
+/**
+ *
+ */
+class Web
+{
 
+    private $app = '';
 
+    public function __construct($app = 'Web')
+    {
+        # code...
+        $this->app = $app;
+    }
 
-
+    public function test()
+    {
+        # code...
+        return $this->app;
+    }
+}
+$app = new Web;
+echo $app->test();
