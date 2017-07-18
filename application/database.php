a05e1bf766805a5a68e4005039256b789147f73e
凉宫长门 <393117228@qq.com>
2017-07-18 12:27:35 +0800

    添加路由支持

--- application/database.php
+++ application/database.php
@@ -15,7 +15,7 @@ return [
     // 服务器地址
     'hostname'        => getenv('OPENSHIFT_MYSQL_DB_HOST') ? : '127.0.0.1',
     // 数据库名
-    'database'        => getenv('OPENSHIFT_APP_NAME') ? : '',
+    'database'        => getenv('OPENSHIFT_APP_NAME') ? : 'test',
     // 用户名
     'username'        => getenv('OPENSHIFT_MYSQL_DB_USERNAME') ? : 'root',
     // 密码
