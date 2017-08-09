3f7f2ef3a09bc9577d3613d6e885b8532a8bcec6
凉宫长门 <393117228@qq.com>
2017-08-09 18:55:36 +0800

    修改

--- application/admin/controller/Rizhi.php
+++ application/admin/controller/Rizhi.php
@@ -9,6 +9,23 @@ class Rizhi extends Controller
     //
     public function index()
     {
+        $list = model('Rizhi')->order('id', 'DESC');
+        $data = [];
+        foreach($list as $da)
+        {
+            $d = json_decode($da['data']);
+            $data[] = [
+                'id'=>$da['id'],
+                'xj1'=>$d['xj1'],
+                'xj2'=>$d['xj2'],
+                'alipay1'=>$d['alipay1'],
+                'alipay2'=>$d['alipay2'],
+                'ce1'=>$d['ce1'],
+                'ce2'=>$d['ce2'],
+                'cdk'=>$d['cdk']
+            ];
+        }
+        $this->assign('rizhi', $data);
         return $this->fetch();
     }
 
