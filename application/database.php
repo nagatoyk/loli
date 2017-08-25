3c61fccdeea8312dd09e362744b49000889afc00
凉宫长门 <393117228@qq.com>
2017-08-26 01:00:29 +0800

    修改

--- application/database.php
+++ application/database.php
@@ -13,15 +13,15 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => getenv('MYSQL_HOST') ? : '127.0.0.1',
+    'hostname'        => MYSQL_HOST,
     // 数据库名
-    'database'        => getenv('MYSQL_DATABASE') ? : 'test',
-    // 用户名 admin8Kc4DPL
-    'username'        => getenv('MYSQL_USER') ? : 'root',
-    // 密码 TAG1H1MeuPIL
-    'password'        => getenv('MYSQL_PASSWORD') ? : '',
+    'database'        => MYSQL_DATABASE,
+    // 用户名
+    'username'        => MYSQL_USER,
+    // 密码
+    'password'        => MYSQL_PASSWORD,
     // 端口
-    'hostport'        => getenv('MYSQL_PORT') ? : 3306,
+    'hostport'        => MYSQL_PORT,
     // 连接dsn
     'dsn'             => '',
     // 数据库连接参数
