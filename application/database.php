f7925322ca93cc2d01ac9ba4d669efea749bdd60
凉宫长门 <393117228@qq.com>
2017-08-28 23:33:37 +0800

    修改

--- application/database.php
+++ application/database.php
@@ -13,15 +13,15 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => getenv('MYSQL_HOST'),
+    'hostname'        => getenv('MYSQL_HOST') ? : '127.0.0.1',
     // 数据库名
-    'database'        => getenv('MYSQL_DATABASE'),
+    'database'        => getenv('MYSQL_DATABASE') ? : 'test',
     // 用户名
-    'username'        => getenv('MYSQL_USERNAME'),
+    'username'        => getenv('MYSQL_USERNAME') ? : 'root',
     // 密码
-    'password'        => getenv('MYSQL_PASSWORD'),
+    'password'        => getenv('MYSQL_PASSWORD') ? : '',
     // 端口
-    'hostport'        => getenv('MYSQL_PORT'),
+    'hostport'        => getenv('MYSQL_PORT') ? : 3306,
     // 连接dsn
     'dsn'             => '',
     // 数据库连接参数
--- application/index/controller/Common.php
+++ application/index/controller/Common.php
@@ -10,11 +10,6 @@ class Common extends Controller
     public function __construct(Request $request = null)
     {
         parent::__construct($request);
-        // if(!model('Category')->where('des', '<>', '')->select())
-        // {
-        	DB::execute(SQL_STR);
-        // }
-        	die();
         $this->assign('cate', model('Category')->where('des','<>','')->order('id','asc')->select());
         $url = 'http://hitoapi.cc/s/?_='.time();
         $json = json_decode(file_get_contents($url), true);
