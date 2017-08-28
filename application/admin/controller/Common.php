ec0c0a6617c197b557e180658e28ec889352fd83
凉宫长门 <393117228@qq.com>
2017-08-29 07:32:20 +0800

    修改

--- application/admin/controller/Common.php
+++ application/admin/controller/Common.php
@@ -10,6 +10,7 @@ class Common extends Controller
 	public function __construct(Request $request = null)
 	{
 		parent::__construct($request);
+		dump($_ENV);
 		// 登录验证
 		if(!session('admin.user_id'))
 		{
