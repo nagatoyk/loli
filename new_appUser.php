c0d9cde141cd5f1ff723f5c69ebfeb8051957ea9
凉宫长门 <393117228@qq.com>
2018-08-20 01:22:23 +0800

    fix2
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- new_appUser.php
+++ new_appUser.php
@@ -4,8 +4,8 @@ require_once './mysql.class.php';
 if (!empty($_POST)) {
     $st = strtotime($_POST['st'] . ' 00:00:00');
     $et = strtotime($_POST['et'] . ' 23:59:59');
-    $list = $sql->getData('select * from app where recTime >= "'.$st.'" and recTime <= "'.$et.'" order by recTime desc');
-    $count = $sql->getVar('select count(*) as cnt from app where recTime >= "'.$st.'" and recTime <= "'.$et.'"');
-    print_r($count);
+    // $list = $sql->getData('select * from app where recTime >= "'.$st.'" and recTime <= "'.$et.'" order by recTime desc');
+    // $count = $sql->getVar('select count(*) as cnt from app where recTime >= "'.$st.'" and recTime <= "'.$et.'"');
+    // print_r($count);
 }
 echo '<pre>'.print_r($sql, true).'</pre>';
