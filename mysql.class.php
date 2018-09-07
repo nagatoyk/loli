fd7a179521df3789b6c03e5ebd3098d719dd2789
凉宫长门 <393117228@qq.com>
2018-09-07 13:03:32 +0800

    fix mysql.class.php

--- mysql.class.php
+++ mysql.class.php
@@ -9,19 +9,19 @@
 require 'DB_driver.php';
 
 $db_driver = 'Mysql';//当前可选driver:Mysql,PDO;
-//if (!empty($_ENV['MYSQL_SERVICE_HOST'])) {
+if (!empty($_ENV['MYSQL_SERVICE_HOST'])) {
     $host = $_ENV['MYSQL_SERVICE_HOST'];
     $port = 3306;
     $dbname = 'sampledb';
     $user = 'shose';
     $pwd = 'xgf520';
-/*} else {
+} else {
     $host = 'localhost';
     $port = 3306;
     $dbname = 'imouto';
     $user = 'root';
     $pwd = 'root';
-}*/
+}
 
 $driver = 'DB_' . $db_driver;
 $sql = new $driver($host, $port, $user, $pwd, $dbname);
