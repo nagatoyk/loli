b12e11c8c2af852c90e928d16f2acdedf5420803
凉宫长门 <393117228@qq.com>
2018-09-07 13:35:54 +0800

    fix list.php

--- list.php
+++ list.php
@@ -1,6 +1,5 @@
 <?php
 require_once './mysql.class.php';
-echo '<pre>' . print_r($sql, true) . '</pre>';
 $sqlText = "CREATE TABLE IF NOT EXISTS `tp_app` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `no` varchar(11) NOT NULL,
@@ -13,6 +12,10 @@ $sqlText = "CREATE TABLE IF NOT EXISTS `tp_app` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0 ;";
 $sql->runSql($sqlText);
+
+$list = $sql->getData('select * from tp_app');
+echo '<pre>' . print_r($list, true) . '</pre>';
+
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'http://e.o2obest.cn/webapp/market/ListMore?page=' . $page . '&startTime=');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
