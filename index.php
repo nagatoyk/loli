f5d72af0039d04f07de0d45db9bfd20907450389
凉宫长门 <393117228@qq.com>
2018-09-07 09:53:55 +0800

    fix index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 echo '<a href="/list.php">新增APP会员查询</a>';
-
+$list = glob('./*');
+echo '<pre>' . print_r($list, true) . '</pre>';
 echo '<pre>' . print_r($_SERVER, true) . '</pre>';
 
 echo '<pre>' . print_r($_ENV, true) . '</pre>';
