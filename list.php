1d77a54f9157bcd82f7535912dfd71d99a7d8f31
凉宫长门 <393117228@qq.com>
2018-09-07 11:25:07 +0800

    fix list.php

--- list.php
+++ list.php
@@ -1,4 +1,6 @@
 <?php
+require_once './mysql.class.php';
+echo '<pre>' . print_r($sql, true) . '</pre>';
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'http://e.o2obest.cn/webapp/market/ListMore?page=' . $page . '&startTime=');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
