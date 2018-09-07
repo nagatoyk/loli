bfa939c862bdce04badfd05a8d9d9fe792af4065
凉宫长门 <393117228@qq.com>
2018-09-07 10:05:24 +0800

    fix list.php

--- list.php
+++ list.php
@@ -1,11 +1,9 @@
 <?php
-require_once './mysql.class.php';
-
-if (!empty($_POST)) {
-    $st = strtotime($_POST['st'] . ' 00:00:00');
-    $et = strtotime($_POST['et'] . ' 23:59:59');
-    // $list = $sql->getData('select * from app where recTime >= "'.$st.'" and recTime <= "'.$et.'" order by recTime desc');
-    // $count = $sql->getVar('select count(*) as cnt from app where recTime >= "'.$st.'" and recTime <= "'.$et.'"');
-    // print_r($count);
-}
-echo '<pre>' . print_r($sql, true) . '</pre>';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'http://e.o2obest.cn/webapp/market/ListMore?page=' . $page . '&startTime=');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_COOKIE, 'USERSESSID=ubi6i93sai8svfmhkhgno68c03');
+$output = curl_exec($ch);
+curl_close($ch);
+$json = json_decode($output, true);
+echo '<pre>' . print_r($json, true) . '</pre>';
